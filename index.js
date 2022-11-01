f74a582cd3aefa097c5a6ee185e66586dc0fdb19
theocita <theocitaallen@gmail.com>
2022-11-01 18:43:49 -0400

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 // Your code here
+function createEmployeeRecord(employee){
+
+    return{
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3], 
+        timeInEvents: [],
+        timeOutEvents: [],
+    
+    }
+}
+
+
+function createEmployeeRecords(employees){
+    return employees.map(employee => createEmployeeRecord(employee))
+}
+
+function createTimeInEvent(employee, dateStamp){
+    let [date, hour] = dateStamp.split(" ") 
+    employee.timeInEvents.push({ 
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date
+    })    
+  return employee
+};
+
+function createTimeOutEvent(employee, dateStamp){
+    let [date, hour] = dateStamp.split(" ") 
+    employee.timeOutEvents.push({ 
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date
+    })    
+  return employee
+}
+
+function hoursWorkedOnDate(employee, date){
+const timeIn = employee.timeInEvents.find(event => event.date === date)
+const timeOut = employee.timeOutEvents.find(event => event.date === date)
+return (timeOut.hour - timeIn.hour)/100
+}
+
+function wagesEarnedOnDate(employee, date){
+    const hours = hoursWorkedOnDate(employee, date)
+    return employee.payPerHour * hours
+}
+
+function allWagesFor(employee){
+   return 
+}
+
+function calculatePayroll(employee){
+    return employee.map(employee => allWagesFor(employee)).reduce((currentValue, total) => currentValue + total)
+}
